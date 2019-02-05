6b099b820a789709d5989f64ccadcbc243d472ac
Sauhaarda Chowdhuri <sauhaarda@gmail.com>
2019-02-05 08:37:10 -0800

    first commit

---
+++ gui.py
@@ -0,0 +1,177 @@
+from tkinter import *
+import os
+from tkinter import filedialog
+from tkinter import Menu
+import tkinter as tk
+import json, torch, librosa, sys
+import torch.utils.data as data
+import numpy as np
+from models import RagaDetector
+
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+# from __future__ import unicode_literals
+import youtube_dl
+
+
+ragas = ['Todi'           ,
+'Puriyā dhanaśrī' ,
+'Rāgēśrī'         ,
+'Mārvā'           ,
+'Yaman kalyāṇ'    ,
+'Bhūp'            ,
+'Miyān malhār'    ,
+'Madhukauns'      ,
+'Madhuvanti'      ,
+'Basant'          ,
+'Khamāj'          ,
+'Kēdār'           ,
+'Lalit'           ,
+'Alahaiyā bilāval',
+'Bhairav'         ,
+'Bihāg'           ,
+'Jōg'             ,
+'Bilāsakhānī tōḍī',
+'Mārūbihāg'       ,
+'Darbāri'         ,
+'Dēś'             ,
+'Ahira bhairav'   ,
+'Śrī'             ,
+'Gauḍ malhār'     ,
+'Ābhōgī'          ,
+'Śuddh sāraṅg'    ,
+'Bairāgi'         ,
+'Mālkauns'        ,
+'Bāgēśrī'         ,
+'Hamsadhvāni']
+
+import matplotlib.pyplot as plt
+
+class popupWindow(object):
+    def __init__(self,master):
+        top=self.top=Toplevel(master)
+        self.top.attributes('-type', 'dialog')
+        self.l=Label(top,text="Enter URL")
+        self.l.pack()
+        self.e=Entry(top)
+        self.e.pack()
+        self.b=Button(top,text='Ok',command=self.cleanup)
+        self.b.pack()
+    def cleanup(self):
+        self.value=self.e.get()
+        self.top.destroy()
+
+class Program:
+    def solve(self, file_name):
+        print('Loading song')
+
+        if(os.path.isfile(file_name.replace('.mp3', '.npy'))):
+            vsong = np.load(file_name.replace('.mp3', '.npy')) 
+        else:
+            y, sr = librosa.load(file_name)
+            vsong = librosa.feature.chroma_stft(y=y, sr=sr, n_fft=4096, hop_length=2048)
+            np.save(file_name.replace('.mp3', ''), vsong)
+
+        print('Done loading song')
+
+        song = torch.from_numpy(vsong).unsqueeze(0).unsqueeze(0).float()
+        model = RagaDetector(0, 256)
+        model.load_state_dict(torch.load('aug2_epoch_665_acc_0.7888888888888889.model', map_location='cpu')['net'])
+        model.eval()
+
+        out = None
+        with torch.no_grad():
+            song_segs = song.split(1500, dim=3)
+            for i in range(len(song_segs)):
+                chunk = song_segs[i] # get specific chunk
+                if not chunk.shape[3] == 1500:  # hardcoded 1500 size
+                    padding = torch.zeros(1, 1, chunk.size()[2], 1500 - chunk.size()[3])
+                    chunk = torch.cat((chunk, padding), 3)
+                if(out is None):
+                   out = model.fc1(model(chunk.contiguous()))
+                   print(out.shape)
+                else:
+                   out += model.fc1(model(chunk.contiguous()))
+
+        confidences = torch.nn.functional.softmax(out, dim=1).numpy()[0]*100
+        print(confidences)
+
+        fig = Figure(figsize=(5,7,), dpi=100)
+        ax = fig.add_subplot(111)
+        ax.bar(ragas, confidences)
+        ax.set_ylim(0, 100)
+        ax.set_ylabel('Prediction Confidence (%)', fontsize=14)
+        ax.set_xlabel('\nRaga', fontsize=14)
+        ax.set_title('Prediction Confidence vs. Raga', fontsize=14)
+        for tick in ax.get_xticklabels():
+            tick.set_rotation(90)
+            tick.set_fontsize(12)
+        fig.tight_layout()
+
+        canvas = FigureCanvasTkAgg(fig, master=self.root)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        toolbar = NavigationToolbar2Tk(canvas, self.root)
+        toolbar.update()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    
+    def youtubeDialog(self):
+        self.w = popupWindow(self.root)
+        self.root.wait_window(self.w.top)
+        url = self.w.value
+        name = url.split('=')[1]
+
+        if not os.path.exists(name + ".mp3"):
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'outtmpl': name + '.%(ext)s',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+            }
+
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+
+        self.solve(name + ".mp3")
+
+    def fileDialog(self):
+        file_name = (filedialog.askopenfilename(initialdir = ".",title = "Select file",filetypes = (("mp3 files","*.mp3"),("all files","*.*"))))
+
+        self.solve(file_name)
+
+
+    def __init__(self):
+        self.root = Tk()
+        self.root.attributes('-type', 'dialog')
+        self.root.title("PhonoNet")
+        self.root.geometry('1000x800')
+        self.menu = Menu(self.root)
+        
+        self.new_item = Menu(self.menu)
+        self.new_item.add_command(label='New Audio File', command=self.fileDialog)
+        self.new_item.add_command(label='New Youtube Song', command=self.youtubeDialog)
+        self.menu.add_cascade(label='File', menu=self.new_item)
+
+        self.root.config(menu=self.menu)
+        self.root.mainloop()
+
+        print('Loading song')
+
+        if(os.path.isfile(file_name.replace('.mp3', '.npy'))):
+            vsong = np.load(file_name.replace('.mp3', '.npy')) 
+        else:
+            y, sr = librosa.load(file_name)
+            vsong = librosa.feature.chroma_stft(y=y, sr=sr, n_fft=4096, hop_length=2048)
+            np.save(file_name.replace('.mp3', ''), vsong)
+
+        print('Done loading song')
+
+        song = torch.from_numpy(vsong).unsqueeze(0).unsqueeze(0).float()
+p = Program()
---
+++ models.py
@@ -0,0 +1,156 @@
+import torch.nn as nn
+import torch
+from collections import OrderedDict
+
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers=2):
+        super(LSTM, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers=num_layers)
+        self.dropout = nn.Dropout(p=0.1)
+        self.hidden = self.init_hidden()
+    def init_hidden(self):
+        return (torch.zeros(self.num_layers, 1, self.hidden_dim),
+                torch.zeros(self.num_layers, 1, self.hidden_dim))
+    def forward(self, x):
+        x =self.dropout(x)
+        lstm_out, self.hidden = self.lstm(x)
+        return lstm_out
+
+
+class RagaDetector(nn.Module):
+    def __init__(self, dropout=0.15, hidden_size=256):
+        super(RagaDetector, self).__init__()
+
+        self.encoder = nn.Sequential(OrderedDict([
+            ('norm0', nn.BatchNorm2d(1)),
+
+            ('conv1', nn.Conv2d(1, 64, 3, padding=1)),
+            ('norm1', nn.BatchNorm2d(64)),
+            ('relu1', nn.LeakyReLU()),
+            ('pool1', nn.MaxPool2d([1, 2])),
+            ('drop1', nn.Dropout(p=dropout)),
+
+            ('conv2', nn.Conv2d(64, 128, 3, padding=1)),
+            ('norm2', nn.BatchNorm2d(128)),
+            ('relu2', nn.LeakyReLU()),
+            ('pool2', nn.MaxPool2d([1, 3])),
+            ('drop2', nn.Dropout(p=dropout)),
+
+            ('conv3', nn.Conv2d(128, 150, 3, padding=1)),
+            ('norm3', nn.BatchNorm2d(150)),
+            ('relu3', nn.LeakyReLU()),
+            ('pool3', nn.MaxPool2d([4, 2])),
+            ('drop3', nn.Dropout(p=dropout)),
+
+            ('conv4', nn.Conv2d(150, 200, 3, padding=1)),
+            ('norm4', nn.BatchNorm2d(200)),
+            ('gba', nn.AvgPool2d([3, 125])),
+            ('drop4', nn.Dropout(p=dropout))
+        ]))
+
+        self.fc1 = nn.Linear(200, 30)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(
+                    m.weight, mode='fan_out', nonlinearity='leaky_relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.encoder(x)
+        batch_size = x.shape[0]
+        x = x.view(batch_size, -1)
+        # x = self.fc1(x)
+        return x
+
+    def summary(self, input_size):
+       	def register_hook(module):
+            def hook(module, input, output):
+                if module._modules:  # only want base layers
+                    return
+                class_name = str(module.__class__).split('.')[-1].split("'")[0]
+                module_idx = len(summary)
+                m_key = '%s-%i' % (class_name, module_idx + 1)
+                summary[m_key] = OrderedDict()
+                summary[m_key]['input_shape'] = list(input[0].size())
+                summary[m_key]['input_shape'][0] = None
+                if output.__class__.__name__ == 'tuple':
+                    summary[m_key]['output_shape'] = list(output[0].size())
+                else:
+                    summary[m_key]['output_shape'] = list(output.size())
+                summary[m_key]['output_shape'][0] = None
+
+                params = 0
+                # iterate through parameters and count num params
+                for name, p in module._parameters.items():
+                    params += torch.numel(p.data)
+                    summary[m_key]['trainable'] = p.requires_grad
+
+                summary[m_key]['nb_params'] = params
+
+            if not isinstance(module, torch.nn.Sequential) and \
+               not isinstance(module, torch.nn.ModuleList) and \
+               not (module == self):
+                hooks.append(module.register_forward_hook(hook))
+
+        # check if there are multiple inputs to the network
+        if isinstance(input_size[0], (list, tuple)):
+            x = [(torch.rand(1, *in_size)) for in_size in input_size]
+        else:
+            x = (torch.randn(1, *input_size))
+
+        # create properties
+        summary = OrderedDict()
+        hooks = []
+        # register hook
+        self.apply(register_hook)
+        # make a forward pass
+        self(x)
+        # remove these hooks
+        for h in hooks:
+            h.remove()
+
+        # print out neatly
+        def get_names(module, name, acc):
+            if not module._modules:
+                acc.append(name)
+            else:
+                for key in module._modules.keys():
+                    p_name = key if name == "" else name + "." + key
+                    get_names(module._modules[key], p_name, acc)
+        names = []
+        get_names(self, "", names)
+
+        col_width = 25  # should be >= 12
+        summary_width = 61
+
+        def crop(s):
+            return s[:col_width] if len(s) > col_width else s
+
+        print('_' * summary_width)
+        print('{0: <{3}} {1: <{3}} {2: <{3}}'.format(
+            'Layer (type)', 'Output Shape', 'Param #', col_width))
+        print('=' * summary_width)
+        total_params = 0
+        trainable_params = 0
+        for (i, l_type), l_name in zip(enumerate(summary), names):
+            d = summary[l_type]
+            total_params += d['nb_params']
+            if 'trainable' in d and d['trainable']:
+                trainable_params += d['nb_params']
+            print('{0: <{3}} {1: <{3}} {2: <{3}}'.format(
+                crop(l_name + ' (' + l_type[:-2] + ')'), crop(str(d['output_shape'])),
+                crop(str(d['nb_params'])), col_width))
+            if i < len(summary) - 1:
+                print('_' * summary_width)
+        print('=' * summary_width)
+        print('Total params: ' + str(total_params))
+        print('Trainable params: ' + str(trainable_params))
+        print('Non-trainable params: ' + str((total_params - trainable_params)))
+        print('_' * summary_width)
